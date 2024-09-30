98b3edc58d5c18292c486c41ee650182d1764233
ValeriiViachalo <valeraviachalo@gmail.com>
2024-09-30 23:04:09 +0300

    list names

--- components/HomePage/HomePage.jsx
+++ components/HomePage/HomePage.jsx
@@ -1,11 +1,22 @@
-import { useState, useEffect } from "react";
 import s from "./HomePage.module.scss";
 import Image from "next/image";
 import content from "./home.json";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export default function HomePage() {
+  const worksRef = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: worksRef.current,
+    offset: ["0% 0%", "100% 100%"],
+    layoutEffect: false,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
+
   return (
-    <div className={s.home}>
+    <div className={s.home} ref={worksRef}>
       <div className={s.home_image_scroll} id="home-scroll">
         <div className={s.home_image_wrapper}>
           {content.works.map((currI, i) => (
@@ -33,6 +44,22 @@ export default function HomePage() {
             ))}
           </div>
         </div>
+      </div>
+      <div className={s.home_names_wrapper}>
+        <motion.li className={s.home_names} style={{ y }}>
+          {content.works.map((currI, i) => (
+            <h1 key={i}>{currI.title}</h1>
+          ))}
+          {content.works.map((currI, i) => (
+            <h1 key={i}>{currI.title}</h1>
+          ))}
+          {content.works.slice(-5).map((currI, i) => (
+            <h1 key={i}>{currI.title}</h1>
+          ))}
+        </motion.li>
+        <span className={s.home_names_visible}>
+          <span className={s.home_names_active}></span>
+        </span>
       </div>
     </div>
   );
