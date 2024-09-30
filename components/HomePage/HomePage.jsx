82b6347992183458f7bf1b870891f6e3260843c6
dimidroupa1 <dimidroupa.react@gmail.com>
2024-09-30 22:06:07 +0300

    small commit

--- components/HomePage/HomePage.jsx
+++ components/HomePage/HomePage.jsx
@@ -1,32 +1,51 @@
-// import { ReactLenis, useLenis } from "lenis/react";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import s from "./HomePage.module.scss";
 import { ScrollProvider } from "@/lib/Providers/ScrollProvider/ScrollProvider";
 
 export default function HomePage() {
-  // useEffect(() => {
-  //   const lenis = new Lenis();
+  const [images, setImages] = useState(new Array(10).fill(null));
+  const scrollRef = useRef(null);
 
-  //   function raf(time) {
-  //     lenis.raf(time);
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
 
-  //     requestAnimationFrame(raf);
-  //   }
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Добавляем новые изображения при достижении конца
+          setImages((prevImages) => [
+            ...prevImages,
+            ...new Array(5).fill(null),
+          ]);
+        }
+      });
+    }, options);
 
-  //   requestAnimationFrame(raf);
-  // }, []);
+    if (scrollRef.current) {
+      observer.observe(scrollRef.current);
+    }
+
+    return () => {
+      if (scrollRef.current) {
+        observer.unobserve(scrollRef.current);
+      }
+    };
+  }, []);
 
   return (
     <div className={s.home}>
-      
-        <div className={s.home_image_scroll} id="home-scroll">
-          <div className={s.home_image_wrapper}>
-            {new Array(10).fill(null).map((_, i) => (
-              <span key={i} className={s.home_image} />
-            ))}
-          </div>
+      <div className={s.home_image_scroll} id="home-scroll">
+        <div className={s.home_image_wrapper}>
+          {images.map((_, i) => (
+            <span key={i} className={s.home_image} />
+          ))}
+          <div ref={scrollRef} style={{ height: "1px" }} />
         </div>
-
+      </div>
     </div>
   );
 }
