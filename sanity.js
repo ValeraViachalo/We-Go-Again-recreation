09ce70c12b6ab699255f12ac60c774c975a32055
dimidroupa1 <dimidroupa.react@gmail.com>
2024-10-19 14:57:14 +0300

    ADD: one env variable

--- sanity.js
+++ sanity.js
@@ -6,8 +6,7 @@ export const config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: "2022-03-07",
   useCdn: false,
-  token:
-    "sk5fs6pegbUQSuus1t3NNVz0RQtXsHfRGauKc3qon3IqFoZ0VuHRPOhb7r8fuu3thyuBraAZ5qwIGDyzTGXvFu27ZKslvtYum2zX5d6Ej5iJk3dAO5qXeCoFAbW7XQqtaSHqUd2OLJG0S959yvOUkzo8nFyEWGw8IXCcr1IIRExnwhw5IsOB",
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 };
 
 export const sanityClient = createClient(config);
